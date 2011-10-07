27c0f4999eb8a616710850d0a318c3efcb2bd8a4
Mathias-S <mathias.stang@gmail.com>
2011-10-07 18:54:30 +0200

    added DepartureViewModel.

---
+++ TronderBuss/ViewModels/DepartureViewModel.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace TronderBuss.ViewModels
+{
+    public class DepartureViewModel : INotifyPropertyChanged
+    {
+        private string line;
+        private string destination;
+        private string registeredDepartureTime;
+        private string scheduledDepartureTime;
+        private bool isRealtimeData;
+
+        public string Line
+        {
+            get { return line; }
+            set
+            {
+                if (line != value)
+                {
+                    line = value;
+                    OnPropertyChange("Line");
+                }
+            }
+        }
+        public string Destination
+        {
+            get { return destination; }
+            set
+            {
+                if (destination != value)
+                {
+                    destination = value;
+                    OnPropertyChange("Destination");
+                }
+            }
+        }
+        public string RegisteredDepartureTime
+        {
+            get { return registeredDepartureTime; }
+            set
+            {
+                if (registeredDepartureTime != value)
+                {
+                    registeredDepartureTime = value;
+                    OnPropertyChange("RegisteredDepartureTime");
+                }
+            }
+        }
+        public string ScheduledDepartureTime
+        {
+            get { return scheduledDepartureTime; }
+            set
+            {
+                if (scheduledDepartureTime != value)
+                {
+                    scheduledDepartureTime = value;
+                    OnPropertyChange("ScheduledDepartureTime");
+                }
+            }
+        }
+        public bool IsRealtimeData
+        {
+            get { return isRealtimeData; }
+            set
+            {
+                if (isRealtimeData != value)
+                {
+                    isRealtimeData = value;
+                    OnPropertyChange("IsRealtimeData");
+                }
+            }
+        }
+
+        private void OnPropertyChange(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
--- TronderBuss/ViewModels/StopViewModel.cs
+++ TronderBuss/ViewModels/StopViewModel.cs
@@ -19,17 +19,82 @@ namespace TronderBuss.ViewModels
             {
                 if (id != value)
                 {
-                    OnPropertyChange("BusStopId");
                     id = value;
+                    OnPropertyChange("BusStopId");
                 }
             }
         }
-        public string Name { get; set; }
-        public string NameWithAbbreviations { get; set; }
-        public string BusStopMaintainer { get; set; }
-        public int LocationId { get; set; }
-        public double Longitude { get; set; }
-        public double Latitude { get; set; }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChange("Name");
+                }
+            }
+        }
+        public string NameWithAbbreviations {
+            get { return nameWithAbbrevations; }
+            set
+            {
+                if (nameWithAbbrevations != value)
+                {
+                    nameWithAbbrevations = value;
+                    OnPropertyChange("NameWithAbbreviations");
+                }
+            }
+        }
+        public string BusStopMaintainer
+        {
+            get { return busStopMaintainer; }
+            set
+            {
+                if (busStopMaintainer != value)
+                {
+                    busStopMaintainer = value;
+                    OnPropertyChange("BusStopMaintainer");
+                }
+            }
+        }
+        public int LocationId
+        {
+            get { return locationId; }
+            set
+            {
+                if (locationId != value)
+                {
+                    locationId = value;
+                    OnPropertyChange("LocationId");
+                }
+            }
+        }
+        public double Longitude
+        {
+            get { return longitude; }
+            set
+            {
+                if (longitude != value)
+                {
+                    longitude = value;
+                    OnPropertyChange("Longitude");
+                }
+            }
+        }
+        public double Latitude
+        {
+            get { return latitude; }
+            set
+            {
+                if (latitude != value)
+                {
+                    latitude = value;
+                    OnPropertyChange("Latitude");
+                }
+            }
+        }
 
         private void OnPropertyChange(string propertyName)
         {
