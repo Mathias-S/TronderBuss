68a877d56ba486009efeb26caf32dc94b01d6193
Mathias-S <mathias.stang@gmail.com>
2011-10-08 00:58:27 +0200

    Rudimentary search support.

--- TronderBuss/MainPage.xaml.cs
+++ TronderBuss/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System;
 using TronderBuss.ViewModels;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 namespace TronderBuss
 {
@@ -46,6 +47,57 @@ namespace TronderBuss
         private string ToQueryString(Dictionary<string,string> nvc)
         {
             return "?" + string.Join("&", nvc.Select(obj => obj.Key + "=" + System.Net.HttpUtility.UrlEncode(obj.Value)));
+        }
+
+        private void stopSearchBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            //TextBox
+        }
+
+        private void SearchTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            HistoryListBox.Visibility = System.Windows.Visibility.Collapsed;
+            HistoryLabel.Visibility = System.Windows.Visibility.Collapsed;
+            SearchListBox.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void SearchTextBox_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            string text = SearchTextBox.Text;
+            StartFilter(text);
+        }
+
+        private void SearchTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(SearchTextBox.Text))
+            {
+                HistoryListBox.Visibility = System.Windows.Visibility.Visible;
+                HistoryLabel.Visibility = System.Windows.Visibility.Visible;
+                SearchListBox.Visibility = System.Windows.Visibility.Collapsed;
+            }
+        }
+
+        Action stop = null;
+        private void StartFilter(string filterText)
+        {
+            if (this.stop != null)
+                this.stop();
+            this.stop = null;
+            bool stop = false;
+            this.stop = () => stop = true;
+            new Thread((ThreadStart)delegate
+            {
+                List<StopGroupViewModel> fin = new List<StopGroupViewModel>();
+                foreach (var item in App.ViewModel.Stops)
+                {
+                    if (stop)
+                        return;
+
+                    if (filterText.ToLower().Split(' ').All(s => item.Name.ToLower().Contains(s)))
+                        fin.Add(item);
+                }
+                Dispatcher.BeginInvoke(() => SearchListBox.ItemsSource = fin);
+            }).Start();
         }
     }
 }
