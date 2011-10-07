dddd36eae3dd1a3b6da6b0292a91eccac2e740d2
Alxandr <alxandr@alxandr.me>
2011-10-07 18:35:08 +0200

    Added StopViewModel.

---
+++ TronderBuss/ViewModels/StopViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+
+namespace TronderBuss.ViewModels
+{
+    public class StopViewModel : INotifyPropertyChanged
+    {
+        private int id;
+        private string name;
+        private string nameWithAbbrevations;
+        private string busStopMaintainer;
+        private int locationId;
+        private double longitude;
+        private double latitude;
+
+        public int BusStopId
+        {
+            get { return id; }
+            set
+            {
+                if (id != value)
+                {
+                    OnPropertyChange("BusStopId");
+                    id = value;
+                }
+            }
+        }
+        public string Name { get; set; }
+        public string NameWithAbbreviations { get; set; }
+        public string BusStopMaintainer { get; set; }
+        public int LocationId { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+
+        private void OnPropertyChange(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
