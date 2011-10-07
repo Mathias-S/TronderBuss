b629b7728d25e665bfeb0d7e87b0bbbed80c9352
Alxandr <alxandr@alxandr.me>
2011-10-07 19:12:36 +0200

    Image?

--- TronderBuss/MainPage.xaml.cs
+++ TronderBuss/MainPage.xaml.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using Microsoft.Phone.Controls;
 
 namespace TronderBuss
@@ -28,10 +18,6 @@ namespace TronderBuss
         // Load data for the ViewModel Items
         private void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
-            if (!App.ViewModel.IsDataLoaded)
-            {
-                App.ViewModel.LoadData();
-            }
         }
     }
 }
